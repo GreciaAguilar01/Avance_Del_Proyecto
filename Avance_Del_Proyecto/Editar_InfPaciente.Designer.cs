f0cc2433ac1294d9a79a80c535e4ab76aa682db5
USUARIO <USUARIO@MIPC>
2026-04-07 19:31:13 -0600

    Agregar archivos de proyecto.

---
+++ Avance_Del_Proyecto/Editar_InfPaciente.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Avance_Del_Proyecto
+{
+    partial class Editar_InfPaciente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.vScrollBarInfPaciente = new System.Windows.Forms.VScrollBar();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AccessibleRole = System.Windows.Forms.AccessibleRole.TitleBar;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(28, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(239, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Historial de Productos:";
+            // 
+            // vScrollBarInfPaciente
+            // 
+            this.vScrollBarInfPaciente.Location = new System.Drawing.Point(23, 82);
+            this.vScrollBarInfPaciente.Name = "vScrollBarInfPaciente";
+            this.vScrollBarInfPaciente.Size = new System.Drawing.Size(574, 305);
+            this.vScrollBarInfPaciente.TabIndex = 1;
+            // 
+            // Editar_InfPaciente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SkyBlue;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.vScrollBarInfPaciente);
+            this.Controls.Add(this.label1);
+            this.Name = "Editar_InfPaciente";
+            this.Text = "Editar_InfPaciente";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.VScrollBar vScrollBarInfPaciente;
+    }
+}

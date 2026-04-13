7a6864b33078dfc3d8f9549957617ed83aeb3752
Frida Velázquez <frifa0608@gmail.com>
2026-04-13 12:01:04 -0600

    Agregué a MASTER el label que faltaba, neta úsenlo pipipip

---
+++ Avance_Del_Proyecto/labelRedondeado.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.ComponentModel;
+
+namespace Avance_Del_Proyecto
+{
+    [ToolboxItem(true)]
+    public class labelRedondeado : Label
+    {
+        private int borderRadius = 20;
+        public int BorderRadius
+        {
+            get => borderRadius;
+            set { borderRadius = value; this.Invalidate(); }
+        }
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+            base.OnPaint(pevent);
+            pevent.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+
+            GraphicsPath path = new GraphicsPath();
+            path.StartFigure();
+            path.AddArc(0, 0, borderRadius, borderRadius, 180, 90);
+            path.AddArc(Width - borderRadius, 0, borderRadius, borderRadius, 270, 90);
+            path.AddArc(Width - borderRadius, Height - borderRadius, borderRadius, borderRadius, 0, 90);
+            path.AddArc(0, Height - borderRadius, borderRadius, borderRadius, 90, 90);
+            path.CloseFigure();
+
+            this.Region = new Region(path);
+
+            using (SolidBrush brush = new SolidBrush(this.BackColor))
+            { pevent.Graphics.FillPath(brush, path); }
+
+            TextRenderer.DrawText(pevent.Graphics, this.Text, this.Font, this.ClientRectangle, this.ForeColor,
+                TextFormatFlags.HorizontalCenter | TextFormatFlags.VerticalCenter);
+
+            using (Pen pen = new Pen(this.Parent.BackColor, 2))
+            {
+                pevent.Graphics.DrawPath(pen, path);
+            }
+        }
+    }
+}

62362fd99bbbd555adeadef8a8b43b75abee5821
grecia <greci@GRECIALAPTOP01>
2026-04-16 13:53:05 -0600

    Edite los botones guardar y cerra

--- Avance_Del_Proyecto/Ingreso_Paciente.Designer.cs
+++ Avance_Del_Proyecto/Ingreso_Paciente.Designer.cs
@@ -48,6 +48,7 @@
             this.PanelBotones = new System.Windows.Forms.Panel();
             this.PanelTitulo = new System.Windows.Forms.Panel();
             this.label10 = new System.Windows.Forms.Label();
+            this.PnIP = new System.Windows.Forms.FlowLayoutPanel();
             this.PanelFormulario.SuspendLayout();
             this.PanelBotones.SuspendLayout();
             this.PanelTitulo.SuspendLayout();
@@ -58,26 +59,29 @@
             this.Label1.AutoSize = true;
             this.Label1.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(223)))), ((int)(((byte)(223)))));
-            this.Label1.Location = new System.Drawing.Point(3, 5);
+            this.Label1.Location = new System.Drawing.Point(4, 6);
+            this.Label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Label1.Name = "Label1";
-            this.Label1.Size = new System.Drawing.Size(172, 23);
+            this.Label1.Size = new System.Drawing.Size(219, 28);
             this.Label1.TabIndex = 0;
             this.Label1.Text = "Nombres del Paciente:";
             // 
             // TBNombrePaciente
             // 
             this.TBNombrePaciente.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(207)))), ((int)(((byte)(176)))));
-            this.TBNombrePaciente.Location = new System.Drawing.Point(8, 34);
+            this.TBNombrePaciente.Location = new System.Drawing.Point(16, 46);
+            this.TBNombrePaciente.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.TBNombrePaciente.Name = "TBNombrePaciente";
-            this.TBNombrePaciente.Size = new System.Drawing.Size(305, 20);
+            this.TBNombrePaciente.Size = new System.Drawing.Size(402, 22);
             this.TBNombrePaciente.TabIndex = 1;
             // 
             // TBFechaNacimiento
             // 
             this.TBFechaNacimiento.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(207)))), ((int)(((byte)(176)))));
-            this.TBFechaNacimiento.Location = new System.Drawing.Point(8, 84);
+            this.TBFechaNacimiento.Location = new System.Drawing.Point(11, 103);
+            this.TBFechaNacimiento.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.TBFechaNacimiento.Name = "TBFechaNacimiento";
-            this.TBFechaNacimiento.Size = new System.Drawing.Size(131, 20);
+            this.TBFechaNacimiento.Size = new System.Drawing.Size(173, 22);
             this.TBFechaNacimiento.TabIndex = 3;
             // 
             // label2
@@ -85,18 +89,20 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(223)))), ((int)(((byte)(223)))));
-            this.label2.Location = new System.Drawing.Point(4, 60);
+            this.label2.Location = new System.Drawing.Point(5, 74);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(257, 23);
+            this.label2.Size = new System.Drawing.Size(327, 28);
             this.label2.TabIndex = 2;
             this.label2.Text = "Fecha de Nacimiento del Paciente:";
             // 
             // TBNumerodelPaciente
             // 
             this.TBNumerodelPaciente.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(207)))), ((int)(((byte)(176)))));
-            this.TBNumerodelPaciente.Location = new System.Drawing.Point(9, 132);
+            this.TBNumerodelPaciente.Location = new System.Drawing.Point(12, 162);
+            this.TBNumerodelPaciente.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.TBNumerodelPaciente.Name = "TBNumerodelPaciente";
-            this.TBNumerodelPaciente.Size = new System.Drawing.Size(131, 20);
+            this.TBNumerodelPaciente.Size = new System.Drawing.Size(173, 22);
             this.TBNumerodelPaciente.TabIndex = 5;
             // 
             // label3
@@ -104,18 +110,20 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(223)))), ((int)(((byte)(223)))));
-            this.label3.Location = new System.Drawing.Point(4, 107);
+            this.label3.Location = new System.Drawing.Point(5, 132);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(220, 23);
+            this.label3.Size = new System.Drawing.Size(276, 28);
             this.label3.TabIndex = 4;
             this.label3.Text = "Numero Celular del Paciente:";
             // 
             // TBCorreoPaciente
             // 
             this.TBCorreoPaciente.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(207)))), ((int)(((byte)(176)))));
-            this.TBCorreoPaciente.Location = new System.Drawing.Point(8, 180);
+            this.TBCorreoPaciente.Location = new System.Drawing.Point(11, 222);
+            this.TBCorreoPaciente.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.TBCorreoPaciente.Name = "TBCorreoPaciente";
-            this.TBCorreoPaciente.Size = new System.Drawing.Size(305, 20);
+            this.TBCorreoPaciente.Size = new System.Drawing.Size(405, 22);
             this.TBCorreoPaciente.TabIndex = 7;
             // 
             // label4
@@ -123,18 +131,20 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Comic Sans MS", 12F);
             this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(223)))), ((int)(((byte)(223)))));
-            this.label4.Location = new System.Drawing.Point(4, 157);
+            this.label4.Location = new System.Drawing.Point(5, 193);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(157, 23);
+            this.label4.Size = new System.Drawing.Size(199, 28);
             this.label4.TabIndex = 6;
             this.label4.Text = "Correo del Paciente:";
             // 
             // TBNombreFamiliar
             // 
             this.TBNombreFamiliar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(207)))), ((int)(((byte)(176)))));
-            this.TBNombreFamiliar.Location = new System.Drawing.Point(10, 225);
+            this.TBNombreFamiliar.Location = new System.Drawing.Point(13, 277);
+            this.TBNombreFamiliar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.TBNombreFamiliar.Name = "TBNombreFamiliar";
-            this.TBNombreFamiliar.Size = new System.Drawing.Size(303, 20);
+            this.TBNombreFamiliar.Size = new System.Drawing.Size(403, 22);
             this.TBNombreFamiliar.TabIndex = 9;
             // 
             // label5
@@ -142,18 +152,20 @@
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Comic Sans MS", 12F);
             this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(223)))), ((int)(((byte)(223)))));
-            this.label5.Location = new System.Drawing.Point(5, 203);
+            this.label5.Location = new System.Drawing.Point(7, 250);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(172, 23);
+            this.label5.Size = new System.Drawing.Size(217, 28);
             this.label5.TabIndex = 8;
             this.label5.Text = "Nombres del Familiar:";
             // 
             // TBNumerodelFamiliar
             // 
             this.TBNumerodelFamiliar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(207)))), ((int)(((byte)(176)))));
-            this.TBNumerodelFamiliar.Location = new System.Drawing.Point(10, 270);
+            this.TBNumerodelFamiliar.Location = new System.Drawing.Point(13, 332);
+            this.TBNumerodelFamiliar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.TBNumerodelFamiliar.Name = "TBNumerodelFamiliar";
-            this.TBNumerodelFamiliar.Size = new System.Drawing.Size(131, 20);
+            this.TBNumerodelFamiliar.Size = new System.Drawing.Size(173, 22);
             this.TBNumerodelFamiliar.TabIndex = 11;
             // 
             // label6
@@ -161,18 +173,20 @@
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Comic Sans MS", 12.25F);
             this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(223)))), ((int)(((byte)(223)))));
-            this.label6.Location = new System.Drawing.Point(5, 246);
+            this.label6.Location = new System.Drawing.Point(7, 303);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(237, 24);
+            this.label6.Size = new System.Drawing.Size(290, 29);
             this.label6.TabIndex = 10;
             this.label6.Text = "Numero Celular del Familiar:";
             // 
             // TBCorreoFamiliar
             // 
             this.TBCorreoFamiliar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(207)))), ((int)(((byte)(176)))));
-            this.TBCorreoFamiliar.Location = new System.Drawing.Point(10, 315);
+            this.TBCorreoFamiliar.Location = new System.Drawing.Point(13, 388);
+            this.TBCorreoFamiliar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.TBCorreoFamiliar.Name = "TBCorreoFamiliar";
-            this.TBCorreoFamiliar.Size = new System.Drawing.Size(303, 20);
+            this.TBCorreoFamiliar.Size = new System.Drawing.Size(403, 22);
             this.TBCorreoFamiliar.TabIndex = 13;
             // 
             // label7
@@ -180,28 +194,36 @@
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Comic Sans MS", 12.25F);
             this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(223)))), ((int)(((byte)(223)))));
-            this.label7.Location = new System.Drawing.Point(5, 290);
+            this.label7.Location = new System.Drawing.Point(7, 357);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(168, 24);
+            this.label7.Size = new System.Drawing.Size(206, 29);
             this.label7.TabIndex = 12;
             this.label7.Text = "Correo del Familiar:";
             // 
             // BtnGuardarPaciente
             // 
             this.BtnGuardarPaciente.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(74)))), ((int)(((byte)(86)))));
-            this.BtnGuardarPaciente.Location = new System.Drawing.Point(22, 14);
+            this.BtnGuardarPaciente.FlatAppearance.BorderSize = 0;
+            this.BtnGuardarPaciente.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnGuardarPaciente.Location = new System.Drawing.Point(29, 17);
+            this.BtnGuardarPaciente.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.BtnGuardarPaciente.Name = "BtnGuardarPaciente";
-            this.BtnGuardarPaciente.Size = new System.Drawing.Size(354, 28);
+            this.BtnGuardarPaciente.Size = new System.Drawing.Size(472, 34);
             this.BtnGuardarPaciente.TabIndex = 15;
             this.BtnGuardarPaciente.Text = "Guardar";
             this.BtnGuardarPaciente.UseVisualStyleBackColor = false;
+            this.BtnGuardarPaciente.Click += new System.EventHandler(this.BtnGuardarPaciente_Click);
             // 
             // BtnBorrarFormulario
             // 
             this.BtnBorrarFormulario.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(114)))), ((int)(((byte)(76)))));
-            this.BtnBorrarFormulario.Location = new System.Drawing.Point(22, 51);
+            this.BtnBorrarFormulario.FlatAppearance.BorderSize = 0;
+            this.BtnBorrarFormulario.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnBorrarFormulario.Location = new System.Drawing.Point(29, 63);
+            this.BtnBorrarFormulario.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.BtnBorrarFormulario.Name = "BtnBorrarFormulario";
-            this.BtnBorrarFormulario.Size = new System.Drawing.Size(354, 28);
+            this.BtnBorrarFormulario.Size = new System.Drawing.Size(472, 34);
             this.BtnBorrarFormulario.TabIndex = 16;
             this.BtnBorrarFormulario.Text = "Borrar";
             this.BtnBorrarFormulario.UseVisualStyleBackColor = false;
@@ -209,8 +231,9 @@
             // PanelFormulario
             // 
             this.PanelFormulario.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(104)))), ((int)(((byte)(95)))));
-            this.PanelFormulario.Controls.Add(this.Label1);
             this.PanelFormulario.Controls.Add(this.TBNombrePaciente);
+            this.PanelFormulario.Controls.Add(this.PnIP);
+            this.PanelFormulario.Controls.Add(this.Label1);
             this.PanelFormulario.Controls.Add(this.label2);
             this.PanelFormulario.Controls.Add(this.TBFechaNacimiento);
             this.PanelFormulario.Controls.Add(this.TBCorreoFamiliar);
@@ -223,9 +246,10 @@
             this.PanelFormulario.Controls.Add(this.TBCorreoPaciente);
             this.PanelFormulario.Controls.Add(this.TBNombreFamiliar);
             this.PanelFormulario.Controls.Add(this.label5);
-            this.PanelFormulario.Location = new System.Drawing.Point(29, 77);
+            this.PanelFormulario.Location = new System.Drawing.Point(39, 95);
+            this.PanelFormulario.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.PanelFormulario.Name = "PanelFormulario";
-            this.PanelFormulario.Size = new System.Drawing.Size(331, 350);
+            this.PanelFormulario.Size = new System.Drawing.Size(441, 431);
             this.PanelFormulario.TabIndex = 17;
             // 
             // PanelBotones
@@ -233,18 +257,20 @@
             this.PanelBotones.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(104)))), ((int)(((byte)(95)))));
             this.PanelBotones.Controls.Add(this.BtnGuardarPaciente);
             this.PanelBotones.Controls.Add(this.BtnBorrarFormulario);
-            this.PanelBotones.Location = new System.Drawing.Point(377, 333);
+            this.PanelBotones.Location = new System.Drawing.Point(503, 410);
+            this.PanelBotones.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.PanelBotones.Name = "PanelBotones";
-            this.PanelBotones.Size = new System.Drawing.Size(398, 94);
+            this.PanelBotones.Size = new System.Drawing.Size(531, 116);
             this.PanelBotones.TabIndex = 18;
             // 
             // PanelTitulo
             // 
             this.PanelTitulo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(31)))), ((int)(((byte)(122)))));
             this.PanelTitulo.Controls.Add(this.label10);
-            this.PanelTitulo.Location = new System.Drawing.Point(12, 10);
+            this.PanelTitulo.Location = new System.Drawing.Point(16, 12);
+            this.PanelTitulo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.PanelTitulo.Name = "PanelTitulo";
-            this.PanelTitulo.Size = new System.Drawing.Size(765, 61);
+            this.PanelTitulo.Size = new System.Drawing.Size(1020, 75);
             this.PanelTitulo.TabIndex = 19;
             // 
             // label10
@@ -253,21 +279,31 @@
             this.label10.BackColor = System.Drawing.Color.Transparent;
             this.label10.Font = new System.Drawing.Font("Cooper Black", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label10.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(207)))), ((int)(((byte)(176)))));
-            this.label10.Location = new System.Drawing.Point(198, 13);
+            this.label10.Location = new System.Drawing.Point(264, 16);
+            this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(370, 27);
+            this.label10.Size = new System.Drawing.Size(462, 35);
             this.label10.TabIndex = 0;
             this.label10.Text = "Ingresar Paciente - Ortopedia";
             // 
+            // PnIP
+            // 
+            this.PnIP.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(142)))), ((int)(((byte)(172)))), ((int)(((byte)(158)))));
+            this.PnIP.Location = new System.Drawing.Point(12, 40);
+            this.PnIP.Name = "PnIP";
+            this.PnIP.Size = new System.Drawing.Size(411, 34);
+            this.PnIP.TabIndex = 14;
+            // 
             // Ingreso_Paciente
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(207)))), ((int)(((byte)(176)))));
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.PanelTitulo);
             this.Controls.Add(this.PanelBotones);
             this.Controls.Add(this.PanelFormulario);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Ingreso_Paciente";
             this.Text = "Ingreso_Paciente";
             this.Load += new System.EventHandler(this.Ingreso_Paciente_Load_1);
@@ -302,5 +338,6 @@
         private System.Windows.Forms.Panel PanelBotones;
         private System.Windows.Forms.Panel PanelTitulo;
         private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.FlowLayoutPanel PnIP;
     }
 }

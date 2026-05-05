f49dd763d65fe967d83a511191598b5e4d1b7a16
frida <frifa0608@gmail.com>
2026-05-05 14:14:42 -0600

    agregué una pinche palabra alv

--- Avance_Del_Proyecto/Inventario.cs
+++ Avance_Del_Proyecto/Inventario.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Drawing.Drawing2D;
+using MySql.Data.MySqlClient;
 
 namespace Avance_Del_Proyecto
 {
@@ -17,6 +18,30 @@ namespace Avance_Del_Proyecto
         {
             InitializeComponent();
         }
+        }
 
+        private void dgvInventario_DataBindingComplete(object sender, DataGridViewBindingCompleteEventArgs e)
+        {
+            dgvInventario.ClearSelection();
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            if (dgvInventario.SelectedRows.Count > 0)
+            {
+                frmDetalleProducto ventana = new frmDetalleProducto();
+                ventana.EsEdicion = true;
+                
+                int id = Convert.ToInt32(dgvInventario.CurrentRow.Cells["ID"].Value);
+                string nombre = dgvInventario.CurrentRow.Cells["nombre"].Value.ToString();
+                int codigo = Convert.ToInt32(dgvInventario.CurrentRow.Cells["Codigo de barras"].Value);
+                int cantidad = Convert.ToInt32(dgvInventario.CurrentRow.Cells["cantidad"].Value);
+                DateTime fecha_ingreso = Convert.ToDateTime(dgvInventario.CurrentRow.Cells["fecha de ingreso"].Value);
+
+                ventana.IdProducto = id;
+                ventana.LlenarCampos(nombre, codigo, cantidad, fecha_ingreso, id);
+                if (ventana.ShowDialog() == DialogResult.OK) { actualizaGrid(); }
+            }
+        }
     }
 }
